2cb52a20f276508bab4a170f83d7ab0331c47ef2
Mark Harder <mark.harder899@gmail.com>
2014-03-27 13:52:31 -0400

    refactor rectangle

--- src/rectangle.rb
+++ src/rectangle.rb
@@ -2,6 +2,7 @@
 # Mark Harder
 # ----------
 # A simple rectangle class
+# contains x and y coordinates, width, and height
 
 class Rectangle
   attr_reader :x, :y, :width, :height
@@ -13,7 +14,9 @@ class Rectangle
     @height = height
   end
 
+  # check if two rectangles are intersecting based on their postions
+  # and sizes
   def intersect? rect
-    !(@x + @width < rect.x || rect.x + rect.width < @x || @y + @height < rect.y || rect.y + rect.height < @y)
+    rect.is_a?(Rectangle) && !(@x + @width < rect.x || rect.x + rect.width < @x || @y + @height < rect.y || rect.y + rect.height < @y)
   end
 end
